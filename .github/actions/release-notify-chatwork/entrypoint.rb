4f9ecfe4f222a8d2868eeaa7464c859e29cd13af
Makoto Henmi <mako.mail.g@gmail.com>
2020-08-25 16:59:55 +0900

    fix

--- .github/actions/release-notify-chatwork/entrypoint.rb
+++ .github/actions/release-notify-chatwork/entrypoint.rb
@@ -3,12 +3,9 @@
 require "json"
 require "octokit"
 
-
-p "GITHUB_REF=#{ENV.fetch("GITHUB_REF")}"
-p "GITHUB_SERVER_URL=#{ENV.fetch("GITHUB_SERVER_URL")}"
-
-
 event_json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(event_json)
+
+p event['repository']['releases_url']
 
 github = Octokit::Client.new(access_token: ENV.fetch["GITHUB_TOKEN"])
